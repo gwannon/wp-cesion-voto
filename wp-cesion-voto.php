5029fdf18a9ae0fdb907f3dd0aea92aa158f383e
Gwannon <monclus.jorge@gmail.com>
2024-07-16 10:05:35 +0200

    Mejoras textos y diseño emails

--- wp-cesion-voto.php
+++ wp-cesion-voto.php
@@ -42,13 +42,17 @@ function wp_cesion_voto_shortcode($params = array(), $content = null) {
       $message = __("Estimado Sr %s<br/><br/>La representación de sus acciones ha sido delegada satisfactoriamente a %s.<br/><br/>Muchas gracias", 'wp-cesion-voto');
 
       if(isset($_POST['Lecanda'])) {
-        echo "<p class='ok'>".sprintf($message, $user[0], "D. Fernando Lecanda Garamendi")."</p>";
-        wp_mail($user[3], __("AMPLIACIÓN DE CAPITAL DE JOLASETA SA", 'wp-cesion-voto'), sprintf($message, $user[0], "D. Fernando Lecanda Garamendi"), $headers);
+        echo "<p class='ok'>".sprintf($message, $user[0], __("D. Fernando Lecanda Garamendi", 'wp-cesion-voto'))."</p>";
+        $message = sprintf($message, $user[0], __("D. Fernando Lecanda Garamendi", 'wp-cesion-voto'));
+        $message = str_replace("[MESSAGE]", $message, file_get_contents(__DIR__."/email.html"));
+        wp_mail($user[3], __("AMPLIACIÓN DE CAPITAL DE JOLASETA SA", 'wp-cesion-voto'), $message, $headers);
         $user[] = "D. Fernando Lecanda Garamendi";
         jolasetaInsertVote($user);
       } else if(isset($_POST['Castellanos'])) {
-        echo "<p class='ok'>".sprintf($message, $user[0], "D. Diego Castellanos Maruri")."</p>";
-        wp_mail($user[3], __("AMPLIACIÓN DE CAPITAL DE JOLASETA SA", 'wp-cesion-voto'), sprintf($message, $user[0], "D. Fernando Lecanda Garamendi"), $headers);
+        echo "<p class='ok'>".sprintf($message, $user[0], __("D. Diego Castellanos Maruri", 'wp-cesion-voto'))."</p>";
+        $message = sprintf($message, $user[0], __("D. Diego Castellanos Maruri", 'wp-cesion-voto'));
+        $message = str_replace("[MESSAGE]", $message, file_get_contents(__DIR__."/email.html"));
+        wp_mail($user[3], __("AMPLIACIÓN DE CAPITAL DE JOLASETA SA", 'wp-cesion-voto'), $message, $headers);
         $user[] = "D. Diego Castellanos Maruri";
         jolasetaInsertVote($user);
       }
@@ -62,8 +66,8 @@ function wp_cesion_voto_shortcode($params = array(), $content = null) {
         <div class="delego"><?php printf(__("Delego %s a:", 'wp-cesion-voto'), ($user[4] > 1 ? "mis ".$user[4]." votos" : "mi voto")); ?></div>
         <form class="delego" method="post" action="<?=get_the_permalink();?>">
           <input type="hidden" name="hash" value="<?=strip_tags($user[5])?>" />
-          <input type="submit" name="Lecanda" value="<?php echo "D. Fernando Lecanda Garamendi"; ?>">
-          <br/><input type="submit" name="Castellanos" value="<?php echo "D. Diego Castellanos Maruri"; ?>">
+          <input type="submit" name="Lecanda" value="<?php _e("D. Fernando Lecanda Garamendi", 'wp-cesion-voto'); ?>">
+          <br/><input type="submit" name="Castellanos" value="<?php _e("D. Diego Castellanos Maruri", 'wp-cesion-voto'); ?>">
           <?php /* <br/><a href="#">DELEGO MI VOTO A OTRO ACCIONISTA DE MI ELECCIÓN</a>*/ ?>
         <form>
         <?php
@@ -82,23 +86,24 @@ function wp_cesion_voto_shortcode($params = array(), $content = null) {
 
             $url = get_the_permalink()."?hash=".$hash;
             $message = sprintf(__("Estimado Sr %s<br/><br/>Gracias por tramitar la delegación de la representación de sus acciones.<br/><br/>Mediante este <a href='%s'>enlace único</a> Ud. podrá delegar la representación de sus acciones a D. Fernando Lecanda Garamendi, con DNI 16036142J, Presidente del Consejo de Administración de Jolaseta S.A; o bien a D. Diego Castellanos Maruri, con DNI 16049155P, Presidente del Real Club Jolaseta.<br/><br/>En el caso de desear delegar la representación de sus acciones a otro accionista de su elección, puede descargar el siguiente <a href='#'>formulario de delegación</a>, cumplimentándolo debidamente y entregándolo en la Administración del Club o bien enviándolo por correo electrónico a <a href='mailto:registro@jolaseta.com'>registro@jolaseta.com</a>.<br/><br/>Muchas gracias", 'wp-cesion-votos'), $user[0], $url);
+            $message = str_replace("[MESSAGE]", $message, file_get_contents(__DIR__."/email.html"));
             $headers = array('Content-Type: text/html; charset=UTF-8');
             wp_mail($_POST['email'], __("AMPLIACIÓN DE CAPITAL DE JOLASETA SA", 'wp-cesion-voto'), $message, $headers);
 
             
-            echo "<p class='advise'>".__("Hemos enviado un email a tu correo con instrucciones para hacer la cesión de votos.", 'wp-cesion-voto')."</p>";
+            echo "<p class='advise'>".__("Hemos enviado un email a su correo con instrucciones para hacer la cesión de votos.", 'wp-cesion-voto')."</p>";
           } else {
-            echo "<p class='error'>".__("Ya has delegado tu voto con anterioridad.", 'wp-cesion-voto')."</p>";
+            echo "<p class='error'>".__("Ya ha delegado su voto con anterioridad.", 'wp-cesion-voto')."</p>";
             $showform = false;
           }
-        } else echo "<p class='error'>".__("No hemos encontrado tus datos en nuestra base de datos.", 'wp-cesion-voto')."</p>";
+        } else echo "<p class='error'>".__("No hemos encontrado sus datos en nuestra base de datos.", 'wp-cesion-voto')."</p>";
       } else echo "<p class='error'>".__("Documento de identificación incorrecto", 'wp-cesion-voto')."</p>";
     } else echo "<p class='error'>".__("Email incorrecto", 'wp-cesion-voto')."</p>";
   }
 
   if($showform) { ?>
     <?=apply_filters("the_content", $content);?>
-    <form method="post">
+    <form method="post" class="logeo">
       <label><b><?php _e("Email", 'wp-cesion-voto'); ?>:</b><br/>
         <input type="email" name="email" value="" required></label><br/>
       <label><b><?php _e("DNI/NIE", 'wp-cesion-voto'); ?>:</b><br/>
@@ -135,6 +140,13 @@ function wp_cesion_voto_shortcode($params = array(), $content = null) {
       padding: 10px 10px 30px 10px;
     }
 
+    form.logeo {
+      display: flex;
+      flex-wrap: wrap;
+      flex-direction: column;
+      align-content: center;
+    }
+
     form.delego {
       display: flex;
       gap: 10px;
@@ -142,8 +154,8 @@ function wp_cesion_voto_shortcode($params = array(), $content = null) {
     }
 
     form.delego input[type=submit] {
-        width: calc(100% - 10px);
-      }
+      width: calc(100% - 10px);
+    }
 
     @media (min-width: 900px) {
       form.delego input[type=submit] {
